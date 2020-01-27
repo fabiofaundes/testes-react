95b0d6c229a5459a53e0e3420b6f49a2f4f0a8c4
fabiofaundes <fabio_faundes@hotmail.com>
2020-01-27 15:12:57 -0300

    Feature -> Menu de navegação
    Feito o estilo e html do menu de navigação

--- src/Nav.js
+++ src/Nav.js
@@ -20,11 +20,41 @@ const Logo = styled.span `
     padding: 5px;
 `
 
+const Menu = styled.div `
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    height: 100%;
+
+    .menu-item {
+        display: flex;
+        align-items: center;
+
+        box-sizing:border-box;
+
+        height: 100%;
+        padding: 0 30px;
+           
+    }
+
+    .menu-item:hover{
+        cursor: pointer;
+        background-color: rgb(255, 255, 102);
+    }
+
+`
+
 class Nav extends React.Component {
     render() {
         return(
             <MyNav>
-                <Logo class='font-arvo'>LOGO</Logo>
+                <Logo class='font-arvo'>Logo</Logo>
+                <Menu>
+                    <a class='menu-item'><span>Home</span></a>
+                    <a class='menu-item'><span>About</span></a>
+                    <a class='menu-item'><span>Contact</span></a>
+                </Menu>
             </MyNav>
         )
     }
